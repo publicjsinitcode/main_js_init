ede46371b1a3f420212193fb830c90a73c3d7374
digital <98893278+digital-diagnostech@users.noreply.github.com>
2022-03-09 14:46:03 +0200

    Create init.js

---
+++ init.js
@@ -0,0 +1,37 @@
+get_files_for_chat_and_include();
+
+function get_files_for_chat_and_include() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/connector-to-analytics.php?type=get_files_chat');
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            // console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            let script = document.createElement('script');
+            let scriptTwo = document.createElement('script');
+
+            let addScripts = new Promise(function (resolve, reject) {
+                script.src = '/services/js/main.js';
+                scriptTwo.src = '/services/js/widgetForm.min.js';
+                document.getElementsByTagName('head')[0].appendChild(script);
+                document.getElementsByTagName('head')[0].appendChild(scriptTwo);
+
+                scriptTwo.onload = function () {
+                    resolve({status: 200});
+                }
+            });
+
+            let appWindow = window;
+            addScripts.then(mew => {
+                if(mew.status) {
+                    appWindow.widgetForm({forms: ['modal-callback-vertical', 'schedule-step', 'callback-vertical', 'connect-with-plumber', 'connect-with-plumber-quote', 'connect-with-plumber-energency', 'callback-vertical-whis-postcode', 'alp-connect-specialist-b-r', 'alp-connect-specialist', 'pro-connect-spec-border', 'pumb-schedule-service-b-r', 'subscribe-form', 'schedule-step-new', 'hvac-modal-form', 'hvac-horizont-form']});
+                }
+            });
+            // console.log(xhr);
+        }
+    };
+    xhr.onerror = function() {
+        // console.log("Запрос не удался");
+    };
+}

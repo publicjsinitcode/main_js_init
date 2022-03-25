2d85d6af857968d2298b69988280a0a5af0e9068
digital <98893278+digital-diagnostech@users.noreply.github.com>
2022-03-25 16:07:01 +0200

    Update init.js

--- init.js
+++ init.js
@@ -8,12 +8,13 @@ function get_files_for_chat_and_include() {
         if (xhr.status != 200) {
             // console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
+            let respJson = JSON.parse(xhr.response);
             let script = document.createElement('script');
             let scriptTwo = document.createElement('script');
 
             let addScripts = new Promise(function (resolve, reject) {
-                script.src = '/services/js/main.js';
-                scriptTwo.src = '/services/js/widgetForm.min.js';
+                script.src = '/services/js/main.js?v='+respJson.resp.js.mainjs;
+                scriptTwo.src = '/services/js/widgetForm.min.js?v='+respJson.resp.js.widgetFormminjs;
                 document.getElementsByTagName('head')[0].appendChild(script);
                 document.getElementsByTagName('head')[0].appendChild(scriptTwo);
 
@@ -25,7 +26,7 @@ function get_files_for_chat_and_include() {
             let appWindow = window;
             addScripts.then(mew => {
                 if(mew.status) {
-                    appWindow.widgetForm({forms: ['modal-form-appliancer-plan', 'modal-callback-vertical', 'schedule-step', 'callback-vertical', 'connect-with-plumber', 'connect-with-plumber-quote', 'connect-with-plumber-energency', 'callback-vertical-whis-postcode', 'alp-connect-specialist-b-r', 'alp-connect-specialist', 'pro-connect-spec-border', 'pumb-schedule-service-b-r', 'subscribe-form', 'schedule-step-new', 'hvac-modal-form', 'hvac-horizont-form']});
+                    appWindow.widgetForm({forms: ['form-appliancer-contact', 'subscribe-form-appliancer', 'modal-form-appliancer-plan', 'modal-callback-vertical', 'schedule-step', 'callback-vertical', 'connect-with-plumber', 'connect-with-plumber-quote', 'connect-with-plumber-energency', 'callback-vertical-whis-postcode', 'alp-connect-specialist-b-r', 'alp-connect-specialist', 'pro-connect-spec-border', 'pumb-schedule-service-b-r', 'subscribe-form', 'schedule-step-new', 'hvac-modal-form', 'hvac-horizont-form']});
                 }
             });
             // console.log(xhr);
